f0f699547ffc628fed350c7b35b9095a1d4267a4
ankesh2005 <ankeshgupta1802@gmail.com>
2026-01-24 22:46:06 +0530

    add error page

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 // Catch-all for wrong/non-existent paths
 app.use((err, req, res, next) => {
   let {statusCode=500,message="something went wrong"}=err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs",{err})
 });
 
 
