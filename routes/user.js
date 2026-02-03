17467ef2947eeaf4db3fd460ce90bfd623e6e76d
ankesh2005 <ankeshgupta1802@gmail.com>
2026-02-04 03:44:19 +0530

    add signup feature

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 import express, { response } from 'express'
 const router=express.Router()
 import {User} from '../models/user.models.js'
+import wrapAsync from '../utils/wrapAsync.js'
 
 router.get("/demouser",async (req,res)=>{
   const fakeUser=new User({
@@ -13,6 +14,24 @@ router.get("/demouser",async (req,res)=>{
 
 router.get("/signup",(req,res)=>{
   res.render("./users/signup.ejs")
+})
+
+router.post("/signup",wrapAsync(async(req,res)=>{
+  try{
+    let {username,email,password}=req.body
+  const newUser=new User({email,username})
+  const registeredUser=await User.register(newUser,password);
+  console.log(registeredUser)
+  req.flash("success","welcome to ApnaGhar!")
+  res.redirect("/listings")
+  }catch(e){
+    req.flash("error",e.message)
+    res.redirect("/signup")
+  }
+}))
+
+router.get("login",async (req,res)=>{
+  res.render("./users/login.ejs")
 })
 
 export default router
