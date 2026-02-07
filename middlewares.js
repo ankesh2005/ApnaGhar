033a1a5df808618c087f89c6c26a682cb76188d5
ankesh2005 <ankeshgupta1802@gmail.com>
2026-02-07 08:03:53 +0530

    add style to navbar

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,16 @@
 export const isLoggedIn=(req,res,next)=>{
+  
   if(!req.isAuthenticated()){
+    req.session.redirectUrl=req.originalUrl;
     req.flash("error","you must login")
     return res.redirect("/login")
+  }
+  next()
+}
+
+export const savedRedirectUrl=(req,res,next)=>{
+  if(req.session.redirectUrl){
+    res.locals.redirectUrl=req.session.redirectUrl
   }
   next()
 }
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import { User } from "../models/user.models.js";
 import wrapAsync from "../utils/wrapAsync.js";
 import passport from "passport";
+import { savedRedirectUrl } from "../middlewares.js";
 
 // router.get("/demouser", async (req, res) => {
 //   const fakeUser = new User({
@@ -24,11 +25,19 @@ router.post(
       let { username, email, password } = req.body;
       const newUser = new User({ email, username });
       const registeredUser = await User.register(newUser, password);
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        } else {
+          req.flash("success", "you are Sign In");
+          return res.redirect("/listings");
+        }
+      });
       req.flash("success", "welcome to ApnaGhar!");
       res.redirect("/listings");
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/signup");
+    } catch (err) {
+      req.flash("error", err.message);
+      return res.redirect("/signup");
     }
   }),
 );
@@ -38,15 +47,26 @@ router.get("/login", async (req, res) => {
 });
 
 router.post(
-  "/login",
+  "/login",savedRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
   async (req, res) => {
-    req.flash("success","welcome back to ApnaGhar! You are Logged in!")
-    res.redirect("/listings")
-  }
+    req.flash("success", "welcome back to ApnaGhar! You are Logged in!");
+    res.redirect(res.locals.redirectUrl||"/listings");
+  },
 );
+
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      next(err);
+    } else {
+      req.flash("success", "you are logged out");
+      res.redirect("/listings");
+    }
+  });
+});
 
 export default router;
