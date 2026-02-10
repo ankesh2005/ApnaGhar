961a8308f533c260ba6d46f9b3a645b802bbf78f
ankesh2005 <ankeshgupta1802@gmail.com>
2026-02-10 17:50:06 +0530

    compact the routes

--- routes/listing.js
+++ routes/listing.js
@@ -1,30 +1,60 @@
-import express  from 'express'
-import wrapAsync from '../utils/wrapAsync.js';
-import { isLoggedIn, isOwner,validateListing } from '../middlewares.js';
-import { index, renderEditForm, renderNewForm, showListing,createListing, updateListing, destroyListing } from '../controllers/listings.js';
-const router=express.Router()
+import express from "express";
+import wrapAsync from "../utils/wrapAsync.js";
+import { isLoggedIn, isOwner, validateListing } from "../middlewares.js";
+import {
+  index,
+  renderEditForm,
+  renderNewForm,
+  showListing,
+  createListing,
+  updateListing,
+  destroyListing,
+} from "../controllers/listings.js";
+const router = express.Router();
 
 
+
+router
+.route("/")
 // index route
-router.get("/",wrapAsync(index))
-
+.get(wrapAsync(index))
+// new listing route
+.post(validateListing, isLoggedIn, wrapAsync(createListing));
+ 
 // new list route
-router.get("/new",isLoggedIn,wrapAsync(renderNewForm))
+router.get("/new", isLoggedIn, wrapAsync(renderNewForm));
 
-// show route
-router.get("/:id",wrapAsync(showListing))
-
-// add listing route
-router.post("/",validateListing,isLoggedIn,wrapAsync(createListing))
+router
+  .route("/:id")
+  // show route
+  .get(wrapAsync(showListing))
+  // update listing
+  .put(isLoggedIn, isOwner, validateListing, wrapAsync(updateListing))
+  // delete listing
+  .delete(isLoggedIn, isOwner, wrapAsync(destroyListing));
 
 // edit listing route
-router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(renderEditForm))
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(renderEditForm));
+
+export default router;
+
+// index route
+// router.get("/",wrapAsync(index))
+
+// add listing route
+// router.post("/",validateListing,isLoggedIn,wrapAsync(createListing))
+
+// show route
+// router.get("/:id", wrapAsync(showListing));
 
 // update listing route
-router.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(updateListing))
+// router.put(
+//   "/:id",
+//   isLoggedIn,
+//   isOwner,
+//   validateListing,
+//   wrapAsync(updateListing),
+// );
 
-// delete listing
-router.delete("/:id",isLoggedIn,isOwner,wrapAsync(destroyListing))
-
-
-export default router
+// // delete listing
+// router.delete("/:id", isLoggedIn, isOwner, wrapAsync(destroyListing));
--- routes/user.js
+++ routes/user.js
@@ -1,31 +1,37 @@
 import express from "express";
-const router = express.Router();
 import wrapAsync from "../utils/wrapAsync.js";
 import passport from "passport";
 import { savedRedirectUrl } from "../middlewares.js";
-import { login, logout, renderLoginForm, renderSignUpForm, signUp } from "../controllers/users.js";
+import {
+  login,
+  logout,
+  renderLoginForm,
+  renderSignUpForm,
+  signUp,
+} from "../controllers/users.js";
 
-// render signup page
-router.get("/signup", renderSignUpForm);
+const router = express.Router();
 
-//  signup
-router.post(
-  "/signup",
-  wrapAsync(signUp),
-);
+router
+  .route("/signup")
+  // render signup page
+  .get(renderSignUpForm)
+  //  signup
+  .post(wrapAsync(signUp));
 
-// render login page
-router.get("/login",renderLoginForm);
-
-// login
-router.post(
-  "/login",savedRedirectUrl,
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true,
-  }),
-  login
-);
+router
+  .route("/login")
+  // render login page
+  .get(renderLoginForm)
+  // login
+  .post(
+    savedRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    login,
+  );
 
 // logout
 router.get("/logout", logout);
