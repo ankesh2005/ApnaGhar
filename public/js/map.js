d0a78465bbe81e220b4790e66e3498d701a95b0b
ankesh2005 <ankeshgupta1802@gmail.com>
2026-02-14 11:40:36 +0530

    show place on map

--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,29 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-L.marker([51.505, -0.09])
+  // Default location (Delhi example)
+let lat = 28.644800;
+let lng = 77.216721;
+
+// If listing has geometry, override default
+if (geometry && geometry.coordinates && geometry.coordinates.length === 2) {
+  lng = geometry.coordinates[0];
+  lat = geometry.coordinates[1];
+}
+
+  const map = L.map("map").setView([lat, lng], 10);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:  '&copy; <a href="http://www.openstreetmap.org/copyright">ApnaGhar</a>'
+  }).addTo(map);
+
+  L.marker([lat, lng])
     .addTo(map)
-    .bindPopup("Sample Airbnb Listing")
+    .bindPopup(` Exact Location after booking`)
     .openPopup();
+
+
+var circle = L.circle([lat,lng], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
