9f3f5a10020421b634720acf3044b2985a35e58d
ankesh2005 <ankeshgupta1802@gmail.com>
2026-02-14 10:27:37 +0530

    initial map setup with leaflet

--- models/listing.models.js
+++ models/listing.models.js
@@ -31,7 +31,18 @@ const listingSchema = new mongoose.Schema(
     owner:{
       type:Schema.Types.ObjectId,
       ref:"User"
-    }
+    },
+    geometry:{
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number], // [longitude, latitude]
+      required: true,
+    },
+  },
   },
   { timestamps: true }
 );
